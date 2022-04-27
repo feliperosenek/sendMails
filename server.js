f6d6b2f8df1381aabe402fb9c11bbd82c4aacb1a
Felipe Rosene Klapsztein <feliperosenek@gmail.com>
2022-04-27 14:16:23 -0200

    config

--- server.js
+++ server.js
@@ -17,9 +17,21 @@ const port = process.env.PORT || 7000;
 
 app.use(bodyParser.json())
 
+var timestamp = new Date();
+            var ano = timestamp.getFullYear();
+            var mes = timestamp.getMonth();
+            var dia = timestamp.getDate();
+            var hora = timestamp.getHours()
+            var minuto = timestamp.getMinutes()
+
+            if(mes <= 9){mes = mes+1; mes = "0"+mes}
+            if(hora <=9){hora="0"+hora}
+            if(minuto <=9){minuto="0"+minuto}
+            var date = ano +"-"+mes+"-"+dia+" "+hora+":"+minuto;
+
 app.post('/', function (req, res) {
   console.log(req.body[0].email +" - "+ req.body[0].event)
-  sequelize.query("UPDATE facebook SET status='" + req.body[0].event + "' WHERE email='" + req.body[0].email + "'")
+  sequelize.query("UPDATE facebook SET data='" + date + "', status='" + req.body[0].event + "' WHERE email='" + req.body[0].email + "'")
   res.status(200).end() 
 });
 
